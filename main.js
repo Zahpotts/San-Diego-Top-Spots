764233ec666419705e951e6636895f4e927cc354
Isaiah <zahpotts123@gmail.com>
2025-03-21 17:17:26 -0400

    Bult using bootstrap

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-// write your code here
-
+    $.getJSON('data.json', function(data) {
+        var tableBody = $('#topSpotsTable tbody');
+        $.each(data, function(index, spot) {
+            var name = $('<td>').text(spot.name);
+            var description = $('<td>').text(spot.description);
+            var locationLink = $('<a>')
+                .attr('href', 'https://www.google.com/maps?q=' + spot.location[0] + ',' + spot.location[1])
+                .attr('target', '_blank')
+                .addClass('btn btn-primary')
+                .addClass('btn-sm')
+                .text('Open in Maps');
+            var location = $('<td>').append(locationLink);
+            var row = $('<tr>').append(name, description, location);
+            tableBody.append(row);
+        });
+    });
 });
